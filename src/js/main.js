105746a3ceffbe464beed9eddc62822f1877354c
Sebastian Walker <info@walkermedia.de>
2016-10-23 01:05:10 +0200

    Sorted datasets before using them

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,14 @@
+// Configuration
+const DATA_PATH = './assets/data.json';
+const ORDER_MODE = 'ALPHABET'; //REVCHRON - Reverse chronological; ALPHABET - Alphabetically
+
+// Library Includes
 window.Vue = require('../../node_modules/vue/dist/vue.js');
 window.VueResource = require('vue-resource');
 window.Zepto = require('jquery');
 window.Lity = require('lity');
 
+// Component Includes
 var AllView = require('./views/AllView.vue');
 var FavouritesView = require('./views/FavouritesView.vue');
 var InfoView = require('./views/InfoView.vue');
@@ -10,6 +16,7 @@ var PageHeader = require('./partials/PageHeader.vue');
 var PageNav = require('./partials/PageNav.vue');
 var PageFooter = require('./partials/PageFooter.vue');
 
+// Vue Setup
 var vm = new Vue({
     el: '#app',
     components: {AllView, FavouritesView, InfoView, PageHeader, PageNav, PageFooter},
@@ -19,8 +26,18 @@ var vm = new Vue({
     },
     methods: {
         fetch() {
-            this.$http.get('assets/data.json').then((data) => {
-                this.filaments = data.data;
+            this.$http.get(DATA_PATH).then((data) => {
+                if(ORDER_MODE === 'REVCHRON'){
+                    this.filaments = data.data.reverse();
+                }else{
+                    this.filaments = data.data.sort(function(a, b){
+                        if ( a.id < b.id )
+                            return -1;
+                        if ( a.id > b.id )
+                            return 1;
+                        return 0;
+                    });
+                }
             });
         },
         changeView(view) {
