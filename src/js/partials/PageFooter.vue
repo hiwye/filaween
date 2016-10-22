cdf9e6970d21ca257f71671298cbc6372d520eda
Sebastian Walker <info@walkermedia.de>
2016-10-23 00:04:20 +0200

    Switched names in footer to be nice :)

--- src/js/partials/PageFooter.vue
+++ src/js/partials/PageFooter.vue
@@ -3,8 +3,8 @@
         <div class="container">
             <div class="content has-text-centered">
                 <p>
-                    <strong>#Filaween</strong> by <a href="http://sebastianwalker.org">Sebastian Walker</a>
-                    and <a href="http://toms3d.org">Thomas Sanladerer</a>.
+                    <strong>#Filaween</strong> by <a href="http://toms3d.org">Thomas Sanladerer</a>
+                    and <a href="http://sebastianwalker.org">Sebastian Walker</a>.
                 </p>
                 <p>
                     <a href="https://walkermedia.de/about/legal.html">
