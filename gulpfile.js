f494937501219bc7f6361b7c71dd07f9b1263831
Sebastian Walker <info@walkermedia.de>
2016-10-22 15:06:08 +0200

    Setup new file structure for use with vue components.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 'use strict';
 
+var fs = require('fs');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserify = require('browserify');
+var vueify = require('vueify');
 
 gulp.task('sass', function () {
-    return gulp.src('./assets/sass/main.scss')
+    return gulp.src('src/sass/main.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./assets/css'));
+        .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('./assets/sass/**/*.scss', ['sass']);
+    gulp.watch('src/sass/**/*.scss', ['sass']);
+    gulp.watch('src/js/**/*', ['js']);
+});
+
+gulp.task('js', function () {
+    return browserify('src/js/main.js')
+        .transform(vueify)
+        .bundle()
+        .pipe(fs.createWriteStream("build/js/main.js"))
 });
