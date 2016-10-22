b4f5803e682fc7912ae6f46ee93b8b74ea5dc7ad
Sebastian Walker <info@walkermedia.de>
2016-10-23 00:03:30 +0200

    Added filtering feature.

--- src/js/views/AllView.vue
+++ src/js/views/AllView.vue
@@ -1,5 +1,5 @@
 <template>
-    <filament-collection searchable="1" filterable="1" v-bind:items="filaments"></filament-collection>
+    <filament-collection searchable="1" v-bind:items="filaments"></filament-collection>
 </template>
 <script>
     var FilamentCollection = require('../components/FilamentCollection.vue');
